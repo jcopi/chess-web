e675b356045fc320020009214a41ae1ba591c5cf
Joel Copi <joel@copi.dev>
2025-06-09 18:59:13 -0500

    Better middleware management

--- main.go
+++ main.go
@@ -2,17 +2,22 @@ package main
 
 import "net/http"
 
+func Handlers(handlers ...http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		for _, handler := range handlers {
+			handler.ServeHTTP(w, r)
+		}
+	})
+}
+
+func corsHeaders(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Cross-Origin-Embedder-Policy", "require-corp")
+	w.Header().Set("Cross-Origin-Opener-Policy", "same-origin")
+}
+
 func main() {
 	mux := http.NewServeMux()
-
-	staticFileHandler := http.FileServerFS(Resources)
-
-	mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Cross-Origin-Embedder-Policy", "require-corp")
-		w.Header().Set("Cross-Origin-Opener-Policy", "same-origin")
-
-		staticFileHandler.ServeHTTP(w, r)
-	}))
+	mux.Handle("/", Handlers(http.HandlerFunc(corsHeaders), http.FileServerFS(Resources)))
 
 	http.ListenAndServe(":http", mux)
 }
